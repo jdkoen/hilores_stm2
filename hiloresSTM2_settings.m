237b04fb23d402fdcc7630e5a444c20ee1bc768d
jdkoen <koen.joshua@gmail.com>
2017-11-04 10:36:30 -0700

    Add hiloresSTM2_settings.m
    Add centralized settings file defining options (timing, display, etc.) for stimulus presentation scripts and input options for subject ID and material sets

--- hiloresSTM2_settings.m
+++ hiloresSTM2_settings.m
@@ -3,8 +3,8 @@
 % in HiLoResSTM2. 
 
 %% Clear screen and workspace
-clear all;
-clearvars -except subID setID monID;
+clearvars -except subID setID setDir monID;
+clc;
 
 %% Get Monitor ID, Subject ID, Material Set ID, and Phase Info
 % Monitor ID
@@ -20,10 +20,13 @@ if ~exist('subID','var')
 end
 
 % Material Set ID
-setID = input('Enter ID for stimulus set (e.g., 101, 102, etc.): ');
-setID = sprintf('set%d',setID);
-setDir = fullfile('stim_sets',setID);
+if ~exist('setID','var') || ~exist('setDir','var')
+    setID = input('Enter ID for stimulus set (e.g., 101, 102, etc.): ');
+    setID = sprintf('set%d',setID);
+    setDir = fullfile('stim_sets',setID);
+end
 if ~exist(setDir,'dir')
+    clear;
     error('stimulus set does not exist.');
 end
 
@@ -34,7 +37,14 @@ if ~ismember(phaseID,[1 2])
     error('Incorrect phase selection. Must be a value of 1 or 2.')
 end
 
-%% Experiment Settings
+%% Common settings and steps for all tasks
+moveOn = 'space';
+KbName('UnifyKeyNames'); % Make sure same across OS
+
+%% Experiment Settings - STM
+% Dimension and rect scalings
+imageDim.x = 150; % Size of the gabor patch in X
+imageDim.y = imageDim.x; % Size of the gabor patch in Y
 textSize = 30; % Size of text on the screen
 baseRect = [0 0 imageDim.x imageDim.y]; % Rectangle for use with psychtoolbox
 xOffset = 100; % Value used to determine the coordinates of 4 quadrants
@@ -47,14 +57,50 @@ duration.stm.st_delay = 1; % Length of delay between study period and first test
 duration.stm.test_isi = .25; % Length of ISI between test probes
 duration.stm.trial_isi = 1.25; % Length of ISI between trials
 
+% Load scale image and set rect
+stm.scaleIMG = imread('lr_scale.png');
+stm.scaleRect = [0 0 432 144];
+
 % Response Options
-moveOn = 'space';
 leftResp = 'f';
 rightResp = 'j';
 
-%% Load Response Scale Image
-% Load scale image
-scaleIMG = imread('lr_scale.png');
+%% Experiment Settings - Gabor
+% Dimension and rect settings
+gabor.x.left = -180; % X position on left
+gabor.x.right = -1*gabor.x.left; % X position on right
+gabor.dim.x = 200; % Size of the gabor patch in X
+gabor.dim.y = gabor.dim.x; % Size of the gabor patch in Y
+gabor.xOffset = 150; % Offset of left and right stims
+gabor.baseREct = [0 0 gabor.dim.x gabor.dim.y];
 
-% Set Rect for response scale image
-scaleRect = [0 0 432 144];
+% Duration variables for Gabor task
+duration.gabor.stim = .3; % Duration of gabor image in seconds
+duration.gabor.iti = .2; % Duration of iti after response in seconds
+
+% Load Gabor image
+gIMG = imread(fullfile('gabors','gabor_08f.png');
+gIMG = imresize(gIMG,[gabor.dim.x gabor.dim.y]);
+
+% Load scale image and set rect
+gabor.scaleIMG = imread('sd_scale.png');
+gabor.scaleRect = [0 0 432 144];
+
+% Response options
+sameResp = 'f';
+diffResp = 'j';
+
+%% Experiment Settings - Perception
+% Dimension and rect scalings
+% Uses the same as the STM task
+
+% Duration variables for perception task
+duration.per.stim = .5; % Duration of stimulus in seconds
+duration.per.iti = 1.25; % Length of ITI in seconds
+
+% Load scale image and set rect
+per.scaleIMG = imread('sd_scale.png');
+per.scaleRect = [0 0 432 144];
+
+% Response options
+% Uses the same as the Gabor
